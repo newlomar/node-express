352840ac98ca7146782adec16e393bfaf94820e1
Newton Lomar <newtonlomar16@gmail.com>
2022-12-08 23:54:44 -0300

    removing endpoints with dishID

--- index.js
+++ index.js
@@ -34,26 +34,6 @@ app.delete('/dishes', (req, res, next) => {
     res.end('Deleting all the dishes!');
 });
 
-app.get('/dishes/:dishId', (req, res, next) => {
-    res.end(`Will send details of the dish: ${req.params.dishId} to you!`);
-});
-
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.statusCode = 403;
-    res.end(`POST operation not supported on /dishes/${req.params.dishId}`);
-});
-
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write(`Updating the dish: ${req.params.dishId}\n`);
-    res.end(
-        `Will Update the dish: ${req.body.name} with details ${req.body.description}`
-    );
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end(`Deleting dish: ${req.params.dishId}`);
-});
-
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
