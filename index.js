a387f15ddd4eaa4800b591005131019865d0ec1c
Newton Lomar <newtonlomar16@gmail.com>
2022-12-09 00:08:52 -0300

    moving dish endpoint to a specific route module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 
+const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -9,31 +10,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.all('/dishers', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
-
-app.get('/dishes', (req, res, next) => {
-    res.end('Will send all the dishes to you!');
-});
-
-app.post('/dishes', (req, res, next) => {
-    res.end(
-        `Will add the dish ${req.body.name} with details ${req.body.description}`
-    );
-});
-
-app.put('/dishes', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /dishes');
-});
-
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all the dishes!');
-});
-
+app.use('/dishes', dishRouter);
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
